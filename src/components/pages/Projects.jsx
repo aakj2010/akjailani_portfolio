168b0de1af9f23fce74230e1e243e1eebe3fae57
aakj2010 <aakj2010@gmail.com>
2024-06-25 23:15:07 +0530

    Comment Project Added

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -4,6 +4,7 @@ import gmail from '../Asset/gmail.png'
 import sof from '../Asset/sof.png'
 import ecomm from '../Asset/ecomm.jpg'
 import behance from '../Asset/behance.png'
+import comment from '../Asset/comment.png'
 import CodeSharpIcon from '@mui/icons-material/CodeSharp';
 import LinkSharpIcon from '@mui/icons-material/LinkSharp';
 import './Pages.css'
@@ -38,6 +39,8 @@ function Projects() {
             </form>
           </div>
 
+         
+
           <div class="work_img col-md-6">
             <img src={gmail} alt="gmail" class="src" />
             <div class="proHead">
@@ -59,7 +62,7 @@ function Projects() {
             </form>
           </div>
 
-          <div class="work_img col-lg-3 col-md-4 col-sm-10" >
+          {/* <div class="work_img col-lg-3 col-md-4 col-sm-10" >
             <img src={ecomm} alt="e-commerce" class="src" />
             <div class="proHead">
               <p class="color"><strong><em>
@@ -76,10 +79,8 @@ function Projects() {
               <a rel="noopener" href="https://github.com/aakj2010/Arima/tree/master/client" target="_blank"><button type="button"
                 class="Code"><CodeSharpIcon fontSize='small' />Github Code</button></a>
 
-              {/* <a rel="noopener" href="https://github.com/aakj2010/Arima/tree/master/backend" target="_blank"><button type="button"
-                class="Code"><CodeSharpIcon fontSize='small' /> Backend</button></a> */}
             </form>
-          </div>
+          </div> */}
 
           <div class="work_img col-md-6">
             <img src={behance} alt="stackoverflow" class="src" />
@@ -121,6 +122,24 @@ function Projects() {
 
               {/* <a rel="noopener noreferrer" href="https://github.com/aakj2010/sofcbd" target="_blank"><button
                 className="Code"><CodeSharpIcon fontSize='small' /> Backend</button></a> */}
+            </form>
+          </div>
+
+          <div class="work_img col-lg-3 col-md-4 col-sm-10" >
+            <img src={comment} alt="Comments" class="src" />
+            <div class="proHead">
+              <p class="color"><strong><em>
+                <h3>Interactive Comment System</h3>
+              </em></strong></p>
+            </div>
+            <div style={{ fontSize: '18px' }}>
+              React/ Custom Hook
+            </div>
+            <form class="project">
+              <a rel="noopener noreferrer" href="https://akj-comments.netlify.app/" target="_blank"><button type="button" class="Live">
+                <LinkSharpIcon fontSize='small' /> View Live Site</button></a>
+              <a rel="noopener noreferrer" href="https://github.com/aakj2010/comments" target="_blank"><button type="button"
+                class="Code"><CodeSharpIcon fontSize='small' />Github Code</button></a>
             </form>
           </div>
 
